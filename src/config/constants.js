ba5db230ab7cb432828434fd03cb911067535cde
André Treib <andre.schumacher.treib@gmail.com>
2020-07-03 20:18:15 -0300

    new api prod source

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,5 @@
-export const VM_URL = "http://localhost:5000/"
+// export const VM_URL = "http://localhost:5000/"
+export const VM_URL = "https://api-barbequeue.azurewebsites.net/"
 export const API_URL = VM_URL
 export const API_LOGIN_KEY = "ca31e4d1102ca53a2826ef95f6e87b21"
 export const CACHED_TOKEN_KEY = "api-key"
